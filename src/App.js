04fa1271201ba2b62d2db9e3a1c49276e9f05661
lucas <lucassouza690@gmail.com>
2021-06-24 21:34:22 -0300

    32. Entendendo props e component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+const BemVindo = (props) => {
+    return(
+        <div>
+            <h2>Bem vindo(a) {props.nome}</h2>
+            <h2>Tenho {props.idade}</h2>
+        </div>
+    );
+};
+
 function App(){
-    return <h1> Ola mundo! </h1>;
+    return <h1> Ola mundo! <BemVindo nome="Lucas" idade="30"></BemVindo></h1>;
 }
 
 export default App;
