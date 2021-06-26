ccfa6d79fa415077fb77bba84db1c83e76e71b08
lucas <lucassouza690@gmail.com>
2021-06-26 09:46:21 -0300

    33. Entendendo props e component pt2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
 
-const BemVindo = (props) => {
-    return(
+const Equipe = (props) =>{
+    return (
         <div>
-            <h2>Bem vindo(a) {props.nome}</h2>
-            <h2>Tenho {props.idade}</h2>
+            <Sobre username={props.nome} cargo={props.cargo} idade={props.idade}/>
+            <Social fb={props.facebook}></Social>
+            <hr></hr>
         </div>
-    );
-};
+    )
+}
+
+const Sobre =(props) =>{
+    return (
+        <div>
+            <h2>ola sou {props.username}</h2>
+            <h3>cargo: {props.cargo}</h3>
+            <h3>tenho: {props.idade} anos</h3>
+        </div>
+    )
+}
+
+const Social =(props) =>{
+    return (
+    <div>
+        <a href={props.fb}>Facebook</a>
+        <a>Linkedin</a>
+        <a>Youtube</a>
+    </div>)
+}
+
 
 function App(){
-    return <h1> Ola mundo! <BemVindo nome="Lucas" idade="30"></BemVindo></h1>;
+    return (
+        <div>
+            <h1> Conheça nossa equipe
+                <Equipe nome="Lucas" cargo="Programador" idade="30" facebook="www.google.com"></Equipe>
+            </h1>
+        </div>
+    );
 }
 
 export default App;
